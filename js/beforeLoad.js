78a1677b1b2bd8dc0118c45b8f3a08debc1eeea2
EdCanCe <edcance.dev@gmail.com>
2024-06-12 22:12:05 -0600

    Mobile detection test

---
+++ js/beforeLoad.js
@@ -0,0 +1,16 @@
+function loadCSS(filename) {
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = filename;
+    document.head.appendChild(link);
+}
+
+let userOS = navigator.userAgent; 
+let mobileDevices = /android|iphone|kindle|ipad/i; 
+let isMobileDevice = mobileDevices.test(userOS); 
+if (isMobileDevice) { 
+    loadCSS('css/mobileMain.css');
+} else { 
+    loadCSS('css/desktopMain.css');
+}
